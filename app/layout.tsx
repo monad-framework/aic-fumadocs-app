0ac19f0dd6248bfcccbd3bda4800bf88d9908d8a
Thomas Carter <thomas.carter@appliedinnovationcorp.com>
2026-08-30 01:32:36 -0400

    feat: mount global site navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import { SiteFooter } from "@/components/site-footer";
+import { SiteFooter } from '@/components/site-footer';
+import { SiteNavbar } from '@/components/site-navbar';
 
 import './global.css';
 import { Inter } from 'next/font/google';
@@ -11,10 +12,13 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex min-h-screen flex-col">
         <RootProvider>
           <div className="flex min-h-screen flex-col">
-            <main className="flex-1">{children}</main>
+            <SiteNavbar />
+            <main id="main-content" tabIndex={-1} className="flex-1 outline-none">
+              {children}
+            </main>
             <SiteFooter />
           </div>
         </RootProvider>
